024cc8a27e2eeecf4327df559dce919e95af1388
tranhung0609 <trvanhung.nhk.0609@gmail.com>
2022-09-14 08:37:37 +0700

    not done

--- src/com/daicent/main/Main.java
+++ src/com/daicent/main/Main.java
@@ -1,9 +1,6 @@
 package com.daicent.main;
 
-import com.daicent.model.Clazz;
-import com.daicent.model.Grade;
-import com.daicent.model.Student;
-import com.daicent.model.Subject;
+import com.daicent.model.*;
 
 import java.util.*;
 
@@ -14,7 +11,9 @@ public class Main {
         List<Clazz> clazzList = new ArrayList<>();
         List<Grade> gradeList = new ArrayList<>();
         List<Subject> subjectList = new ArrayList<>();
+        List<School> schoolList = new ArrayList<>();
         Map<Grade, List<Student>> gradeListMap = new HashMap<>();
+        Map<School, List<Student>> schoolListMap = new HashMap<School, List<Student>>();
         Student student1 = new Student( "Hưng", 24, 5.5);
         Student student2 = new Student("Thuận", 54, 5);
         Student student3 = new Student( "Sơn", 44, 4.6);
@@ -32,6 +31,19 @@ public class Main {
 
         List<Student> studentList1 = Arrays.asList(student1, student2);
         gradeListMap.put(grade1, studentList1);
+        gradeListMap.put(grade2, studentList1);
+        System.out.println("-----------------------");
         System.out.println(gradeListMap);
+        System.out.println("-----------------------");
+
+        School school1 = new School(1, "Daicent");
+        School school2 = new School(2, "FPT");
+        schoolList.add(school1);
+        schoolList.add(school2);
+        schoolListMap.put(school1, studentList);
+        System.out.println(schoolListMap.values());
+        System.out.println("---------------");
+        System.out.println(schoolList);
+
     }
 }
